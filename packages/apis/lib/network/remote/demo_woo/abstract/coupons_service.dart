18192a7201dcce7ccdfc8735c9456a8861fa0821
melihalkbk <melihalkbk@gmail.com>
2025-07-17 01:43:43 +0300

    refactor: remove abstract CouponsService for coupon fetching

--- packages/apis/lib/network/remote/demo_woo/abstract/coupons_service.dart
+++
@@ -1,8 +0,0 @@
-import 'package:apis/network/remote/demo_woo/freezed_model/list_all_coupons_response.dart';
-
-/// 🔑 Abstract contract for Access Scope Service
-/// Implement this to fetch access scopes from Shopify API! 🌐
-abstract class CouponsService {
-  /// 🚀 Fetches the access scope from the API.
-  Future<List<ListAllCouponsResponse>> listAllCoupons();
-}
