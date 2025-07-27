9b2df85720fff9e593fe8afbc87c0d3cb4fdc555
Sultan Kayı <sultankayi01@gmail.com>
2025-07-27 21:50:51 +0300

    refactor: update RetrieveTopSellerReportResponse model and JSON parsing methods

--- packages/apis/lib/network/remote/woocommerce/reports/freezed_model/response/retrieve_top_seller_report_response.dart
+++ packages/apis/lib/network/remote/woocommerce/reports/freezed_model/response/retrieve_top_seller_report_response.dart
@@ -1,17 +1,32 @@
 
+// To parse this JSON data, do
+//
+//     final retrieveTopSellerReportResponse = retrieveTopSellerReportResponseFromJson(jsonString);
+
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'dart:convert';
 
 part 'retrieve_top_seller_report_response.freezed.dart';
 part 'retrieve_top_seller_report_response.g.dart';
 
+List<RetrieveTopSellerReportResponse> retrieveTopSellerReportResponseFromJson(String str) => List<RetrieveTopSellerReportResponse>.from(json.decode(str).map((x) => RetrieveTopSellerReportResponse.fromJson(x)));
+
+String retrieveTopSellerReportResponseToJson(List<RetrieveTopSellerReportResponse> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
 @freezed
 class RetrieveTopSellerReportResponse with _$RetrieveTopSellerReportResponse {
   const factory RetrieveTopSellerReportResponse({
-    required int product_id,
-    required String product_name,
-    required int quantity,
-    required double total,
+    @JsonKey(name: "product_id")
+    int? productId,
+    @JsonKey(name: "product_name") 
+    String? productName,
+    @JsonKey(name: "quantity")
+    int? quantity,
+    @JsonKey(name: "total")
+    String? total,
+    @JsonKey(name: "sku")
     String? sku,
+    @JsonKey(name: "category")
     String? category,
   }) = _RetrieveTopSellerReportResponse;
 
