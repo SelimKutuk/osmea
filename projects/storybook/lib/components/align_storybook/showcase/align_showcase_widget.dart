8f6c58ef471856b08b5d1b098a2b4eaba9eac8a2
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2025-09-05 00:00:46 +0300

    remove: eliminate the properties section and related methods from AlignShowcaseWidget to simplify the component structure

--- projects/storybook/lib/components/align_storybook/showcase/align_showcase_widget.dart
+++ projects/storybook/lib/components/align_storybook/showcase/align_showcase_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:osmea_components/osmea_components.dart';
-import '../data/align_data.dart';
 import '../sections/header_section.dart';
 
 class AlignShowcaseWidget extends StatelessWidget {
@@ -31,11 +30,6 @@ class AlignShowcaseWidget extends StatelessWidget {
 
             // Live Example Section
             _buildDemo(context),
-
-            const SizedBox(height: 32),
-
-            // Properties Section
-            _buildProperties(context),
           ],
         ),
       ),
@@ -98,96 +92,6 @@ class AlignShowcaseWidget extends StatelessWidget {
                 alignment: alignment,
                 child: child,
               ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildProperties(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(24),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16),
-        border: Border.all(
-          color: Colors.grey.shade200,
-          width: 1,
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withValues(alpha: 0.05),
-            blurRadius: 20,
-            offset: const Offset(0, 4),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Icon(
-                Icons.settings,
-                size: 20,
-                color: Colors.grey.shade600,
-              ),
-              const SizedBox(width: 8),
-              Text(
-                'Current Properties',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.grey.shade800,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 20),
-          Container(
-            padding: const EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              color: Colors.grey.shade50,
-              borderRadius: BorderRadius.circular(12),
-              border: Border.all(
-                color: Colors.grey.shade200,
-                width: 1,
-              ),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Alignment',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.grey.shade600,
-                  ),
-                ),
-                Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-                  decoration: BoxDecoration(
-                    color: Colors.blue.shade50,
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all(
-                      color: Colors.blue.shade200,
-                      width: 1,
-                    ),
-                  ),
-                  child: Text(
-                    getAlignmentLabel(alignment),
-                    style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.blue.shade700,
-                      fontFamily: 'monospace',
-                    ),
-                  ),
-                ),
-              ],
             ),
           ),
         ],
