593725104bf47c9a0f252451e63253963882dae3
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2025-09-18 05:45:03 +0300

    feat: add PasswordUpdate method to WooAuthService for secure password updates with JWT authentication

--- packages/apis/lib/network/remote/woocommerce/auth/abstract/woo_auth_service.dart
+++ packages/apis/lib/network/remote/woocommerce/auth/abstract/woo_auth_service.dart
@@ -1,8 +1,10 @@
 import 'package:apis/network/remote/woocommerce/auth/freezed_model/request/delete_user_request.dart';
+import 'package:apis/network/remote/woocommerce/auth/freezed_model/request/password_update_request.dart';
 import 'package:apis/network/remote/woocommerce/auth/freezed_model/request/send_reset_password_request.dart';
 import 'package:apis/network/remote/woocommerce/auth/freezed_model/request/user_login_request.dart';
 import 'package:apis/network/remote/woocommerce/auth/freezed_model/request/user_signup_request.dart';
 import 'package:apis/network/remote/woocommerce/auth/freezed_model/response/delete_user_response.dart';
+import 'package:apis/network/remote/woocommerce/auth/freezed_model/response/password_update_response.dart';
 import 'package:apis/network/remote/woocommerce/auth/freezed_model/response/send_reset_password_response.dart';
 import 'package:apis/network/remote/woocommerce/auth/freezed_model/response/user_login_response.dart';
 import 'package:apis/network/remote/woocommerce/auth/freezed_model/response/user_signup_response.dart';
@@ -29,6 +31,11 @@ abstract class WooAuthService {
   /// Sends password reset email to user
   Future<SendResetPasswordResponse> sendResetPasswordMail(
       String brandName, SendResetPasswordRequest request);
+
+  /// 🔐 Update Password
+  /// Updates user password with JWT authentication
+  Future<PasswordUpdateResponse> updatePassword(
+      String brandName, PasswordUpdateRequest request);
 
   Future<UserLoginResponse> userLoginWithQuery(
       String email, String password, String brandName);
