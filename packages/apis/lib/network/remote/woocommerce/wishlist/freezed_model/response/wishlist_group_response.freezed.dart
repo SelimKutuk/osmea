91dad711bfbe669ba84b26ee8d2f2f7d273dcb9f
Sultan Kayı <sultankayi01@gmail.com>
2025-09-20 15:40:29 +0300

    feat: add WishlistGroupResponse model with JSON serialization

---
+++ packages/apis/lib/network/remote/woocommerce/wishlist/freezed_model/response/wishlist_group_response.freezed.dart
@@ -0,0 +1,322 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'wishlist_group_response.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+WishlistGroupResponse _$WishlistGroupResponseFromJson(
+    Map<String, dynamic> json) {
+  return _WishlistGroupResponse.fromJson(json);
+}
+
+/// @nodoc
+mixin _$WishlistGroupResponse {
+  int? get id => throw _privateConstructorUsedError;
+  String? get name => throw _privateConstructorUsedError;
+  String? get description => throw _privateConstructorUsedError;
+  @JsonKey(name: 'is_default')
+  bool? get isDefault => throw _privateConstructorUsedError;
+  @JsonKey(name: 'item_count')
+  int? get itemCount => throw _privateConstructorUsedError;
+  @JsonKey(name: 'created_at')
+  String? get createdAt => throw _privateConstructorUsedError;
+  @JsonKey(name: 'updated_at')
+  String? get updatedAt => throw _privateConstructorUsedError;
+  @JsonKey(name: 'user_id')
+  int? get userId => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $WishlistGroupResponseCopyWith<WishlistGroupResponse> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $WishlistGroupResponseCopyWith<$Res> {
+  factory $WishlistGroupResponseCopyWith(WishlistGroupResponse value,
+          $Res Function(WishlistGroupResponse) then) =
+      _$WishlistGroupResponseCopyWithImpl<$Res, WishlistGroupResponse>;
+  @useResult
+  $Res call(
+      {int? id,
+      String? name,
+      String? description,
+      @JsonKey(name: 'is_default') bool? isDefault,
+      @JsonKey(name: 'item_count') int? itemCount,
+      @JsonKey(name: 'created_at') String? createdAt,
+      @JsonKey(name: 'updated_at') String? updatedAt,
+      @JsonKey(name: 'user_id') int? userId});
+}
+
+/// @nodoc
+class _$WishlistGroupResponseCopyWithImpl<$Res,
+        $Val extends WishlistGroupResponse>
+    implements $WishlistGroupResponseCopyWith<$Res> {
+  _$WishlistGroupResponseCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? name = freezed,
+    Object? description = freezed,
+    Object? isDefault = freezed,
+    Object? itemCount = freezed,
+    Object? createdAt = freezed,
+    Object? updatedAt = freezed,
+    Object? userId = freezed,
+  }) {
+    return _then(_value.copyWith(
+      id: freezed == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int?,
+      name: freezed == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String?,
+      description: freezed == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String?,
+      isDefault: freezed == isDefault
+          ? _value.isDefault
+          : isDefault // ignore: cast_nullable_to_non_nullable
+              as bool?,
+      itemCount: freezed == itemCount
+          ? _value.itemCount
+          : itemCount // ignore: cast_nullable_to_non_nullable
+              as int?,
+      createdAt: freezed == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as String?,
+      updatedAt: freezed == updatedAt
+          ? _value.updatedAt
+          : updatedAt // ignore: cast_nullable_to_non_nullable
+              as String?,
+      userId: freezed == userId
+          ? _value.userId
+          : userId // ignore: cast_nullable_to_non_nullable
+              as int?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$WishlistGroupResponseImplCopyWith<$Res>
+    implements $WishlistGroupResponseCopyWith<$Res> {
+  factory _$$WishlistGroupResponseImplCopyWith(
+          _$WishlistGroupResponseImpl value,
+          $Res Function(_$WishlistGroupResponseImpl) then) =
+      __$$WishlistGroupResponseImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {int? id,
+      String? name,
+      String? description,
+      @JsonKey(name: 'is_default') bool? isDefault,
+      @JsonKey(name: 'item_count') int? itemCount,
+      @JsonKey(name: 'created_at') String? createdAt,
+      @JsonKey(name: 'updated_at') String? updatedAt,
+      @JsonKey(name: 'user_id') int? userId});
+}
+
+/// @nodoc
+class __$$WishlistGroupResponseImplCopyWithImpl<$Res>
+    extends _$WishlistGroupResponseCopyWithImpl<$Res,
+        _$WishlistGroupResponseImpl>
+    implements _$$WishlistGroupResponseImplCopyWith<$Res> {
+  __$$WishlistGroupResponseImplCopyWithImpl(_$WishlistGroupResponseImpl _value,
+      $Res Function(_$WishlistGroupResponseImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? name = freezed,
+    Object? description = freezed,
+    Object? isDefault = freezed,
+    Object? itemCount = freezed,
+    Object? createdAt = freezed,
+    Object? updatedAt = freezed,
+    Object? userId = freezed,
+  }) {
+    return _then(_$WishlistGroupResponseImpl(
+      id: freezed == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int?,
+      name: freezed == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String?,
+      description: freezed == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String?,
+      isDefault: freezed == isDefault
+          ? _value.isDefault
+          : isDefault // ignore: cast_nullable_to_non_nullable
+              as bool?,
+      itemCount: freezed == itemCount
+          ? _value.itemCount
+          : itemCount // ignore: cast_nullable_to_non_nullable
+              as int?,
+      createdAt: freezed == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as String?,
+      updatedAt: freezed == updatedAt
+          ? _value.updatedAt
+          : updatedAt // ignore: cast_nullable_to_non_nullable
+              as String?,
+      userId: freezed == userId
+          ? _value.userId
+          : userId // ignore: cast_nullable_to_non_nullable
+              as int?,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$WishlistGroupResponseImpl implements _WishlistGroupResponse {
+  const _$WishlistGroupResponseImpl(
+      {this.id,
+      this.name,
+      this.description,
+      @JsonKey(name: 'is_default') this.isDefault,
+      @JsonKey(name: 'item_count') this.itemCount,
+      @JsonKey(name: 'created_at') this.createdAt,
+      @JsonKey(name: 'updated_at') this.updatedAt,
+      @JsonKey(name: 'user_id') this.userId});
+
+  factory _$WishlistGroupResponseImpl.fromJson(Map<String, dynamic> json) =>
+      _$$WishlistGroupResponseImplFromJson(json);
+
+  @override
+  final int? id;
+  @override
+  final String? name;
+  @override
+  final String? description;
+  @override
+  @JsonKey(name: 'is_default')
+  final bool? isDefault;
+  @override
+  @JsonKey(name: 'item_count')
+  final int? itemCount;
+  @override
+  @JsonKey(name: 'created_at')
+  final String? createdAt;
+  @override
+  @JsonKey(name: 'updated_at')
+  final String? updatedAt;
+  @override
+  @JsonKey(name: 'user_id')
+  final int? userId;
+
+  @override
+  String toString() {
+    return 'WishlistGroupResponse(id: $id, name: $name, description: $description, isDefault: $isDefault, itemCount: $itemCount, createdAt: $createdAt, updatedAt: $updatedAt, userId: $userId)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$WishlistGroupResponseImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.description, description) ||
+                other.description == description) &&
+            (identical(other.isDefault, isDefault) ||
+                other.isDefault == isDefault) &&
+            (identical(other.itemCount, itemCount) ||
+                other.itemCount == itemCount) &&
+            (identical(other.createdAt, createdAt) ||
+                other.createdAt == createdAt) &&
+            (identical(other.updatedAt, updatedAt) ||
+                other.updatedAt == updatedAt) &&
+            (identical(other.userId, userId) || other.userId == userId));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, id, name, description, isDefault,
+      itemCount, createdAt, updatedAt, userId);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$WishlistGroupResponseImplCopyWith<_$WishlistGroupResponseImpl>
+      get copyWith => __$$WishlistGroupResponseImplCopyWithImpl<
+          _$WishlistGroupResponseImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$WishlistGroupResponseImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _WishlistGroupResponse implements WishlistGroupResponse {
+  const factory _WishlistGroupResponse(
+          {final int? id,
+          final String? name,
+          final String? description,
+          @JsonKey(name: 'is_default') final bool? isDefault,
+          @JsonKey(name: 'item_count') final int? itemCount,
+          @JsonKey(name: 'created_at') final String? createdAt,
+          @JsonKey(name: 'updated_at') final String? updatedAt,
+          @JsonKey(name: 'user_id') final int? userId}) =
+      _$WishlistGroupResponseImpl;
+
+  factory _WishlistGroupResponse.fromJson(Map<String, dynamic> json) =
+      _$WishlistGroupResponseImpl.fromJson;
+
+  @override
+  int? get id;
+  @override
+  String? get name;
+  @override
+  String? get description;
+  @override
+  @JsonKey(name: 'is_default')
+  bool? get isDefault;
+  @override
+  @JsonKey(name: 'item_count')
+  int? get itemCount;
+  @override
+  @JsonKey(name: 'created_at')
+  String? get createdAt;
+  @override
+  @JsonKey(name: 'updated_at')
+  String? get updatedAt;
+  @override
+  @JsonKey(name: 'user_id')
+  int? get userId;
+  @override
+  @JsonKey(ignore: true)
+  _$$WishlistGroupResponseImplCopyWith<_$WishlistGroupResponseImpl>
+      get copyWith => throw _privateConstructorUsedError;
+}
