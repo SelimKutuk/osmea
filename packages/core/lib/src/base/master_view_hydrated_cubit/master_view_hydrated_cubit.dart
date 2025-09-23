046538aec9d8d7fa5f10bf47848576b5b78207e0
Gürkan Fikret Günak <52853374+gurkanfikretgunak@users.noreply.github.com>
2025-09-23 17:15:50 +0300

    chore: downgrade flutter_bloc to version 9.1.0, add hydrated_bloc dependency, and update component path in pubspec.yaml + add : hydrated feature for cubit view

---
+++ packages/core/lib/src/base/master_view_hydrated_cubit/master_view_hydrated_cubit.dart
@@ -0,0 +1,221 @@
+library master_view_hydrated_cubit;
+
+import 'package:core/core.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:core/src/helper/grid_helper.dart';
+
+part 'master_view_hydrated_cubit_enums.dart';
+part 'master_view_hydrated_cubit_mixins.dart';
+
+/// Hydrated Cubit-based Master View
+abstract class MasterViewHydratedCubit<V extends BaseViewModelHydratedCubit<S>, S>
+    extends StatelessWidget with MasterViewHydratedCubitMixin {
+  final Map<String, dynamic> arguments;
+  final MasterViewHydratedCubitTypes currentView;
+  final Function snackBarFunction;
+  final PreferredSizeWidget Function(BuildContext, V)? coreAppBar;
+  final Widget? Function(BuildContext, V)? coreBottomBar;
+  final bool showDevGrid;
+  final Function(String path) goRoute;
+
+  final Widget? bottomNavigationBar;
+
+  MasterViewHydratedCubit({
+    super.key,
+    this.arguments = const {},
+    this.currentView = MasterViewHydratedCubitTypes.content,
+    this.snackBarFunction = defaultSnackBarFunction,
+    this.coreAppBar,
+    this.coreBottomBar,
+    this.showDevGrid = true,
+    this.bottomNavigationBar,
+    required this.goRoute,
+  }) : assert(arguments.isNotEmpty, 'Arguments must not be empty') {
+    FlutterError.onError = (FlutterErrorDetails details) {
+      debugPrint('FlutterError: ${details.exception}');
+      debugPrintStack(stackTrace: details.stack);
+    };
+  }
+
+  final GlobalKey<ScaffoldMessengerState> _scaffoldMessengerKey =
+      GlobalKey<ScaffoldMessengerState>();
+
+  final ValueNotifier<bool> _didCallInitial = ValueNotifier<bool>(false);
+
+  Widget viewContent(BuildContext context, V viewModel, S state);
+
+  void initialContent(V viewModel, BuildContext context);
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('MasterViewHydratedCubit build. -> View Type: $currentView');
+
+    if (currentView != MasterViewHydratedCubitTypes.content) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        try {
+          final snackBar = _createSnackBar(currentView);
+          _showSnackBar(context, snackBar);
+        } catch (e) {
+          debugPrint('Error creating or showing Snackbar: $e');
+        }
+      });
+    }
+
+    try {
+      return _scaffold(context);
+    } on Exception catch (e, s) {
+      debugPrint('Exception in MasterViewHydratedCubit build: $e');
+      debugPrintStack(stackTrace: s);
+      return _buildErrorScaffold(context, 'Exception: $e');
+    } catch (e, s) {
+      debugPrint('Unknown error in MasterViewHydratedCubit build: $e');
+      debugPrintStack(stackTrace: s);
+      return _buildErrorScaffold(context, 'Unknown error: $e');
+    }
+  }
+
+  Widget _scaffold(BuildContext context) {
+    return _handleScaffoldErrors(() {
+      return BaseViewHydratedCubit<V, S>(
+        onViewModelReady: (viewModel) {
+          if (!_didCallInitial.value) {
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              if (!_didCallInitial.value) {
+                try {
+                  final ctx = _scaffoldMessengerKey.currentContext;
+                  if (ctx != null) {
+                    initialContent(viewModel, ctx);
+                  }
+                } catch (e, s) {
+                  debugPrint('initialContent error: $e');
+                  debugPrintStack(stackTrace: s);
+                } finally {
+                  _didCallInitial.value = true;
+                }
+              }
+            });
+          }
+        },
+        builder: (viewModel, context, state) {
+          return Scaffold(
+            extendBody: true,
+            extendBodyBehindAppBar: true,
+            key: _scaffoldMessengerKey,
+            appBar: coreAppBar?.call(context, viewModel),
+            body: SafeArea(
+              child: Column(
+                children: [
+                  const CoreSpacer(CoreSpacerType.navbar),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: GridHelper.defaultMargin),
+                      child: viewContent(context, viewModel, state),
+                    ),
+                  ),
+                  const CoreSpacer(CoreSpacerType.footer),
+                ],
+              ),
+            ),
+            bottomNavigationBar: coreBottomBar != null
+                ? coreBottomBar!.call(context, viewModel)
+                : bottomNavigationBar,
+          );
+        },
+      );
+    }, context);
+  }
+
+  Widget _handleScaffoldErrors(Function() scaffoldBuilder, BuildContext context) {
+    try {
+      return scaffoldBuilder();
+    } catch (e, s) {
+      debugPrint('Error in scaffold: $e');
+      debugPrintStack(stackTrace: s);
+      return _buildErrorScaffold(context, 'Error: $e');
+    }
+  }
+
+  Widget _buildErrorScaffold(BuildContext context, String message) {
+    return _createScaffold(
+      body: buildError(message),
+    );
+  }
+
+  Widget _createScaffold({required Widget body}) {
+    return Scaffold(
+      key: _scaffoldMessengerKey,
+      backgroundColor: OsmeaColors.white,
+      body: body,
+    );
+  }
+
+  String _getSnackbarMessage(MasterViewHydratedCubitTypes state) {
+    final message = _getMessageForState(state);
+    return message.isNotEmpty
+        ? message
+        : 'An unexpected error occurred. Please try again later.';
+  }
+
+  String _getMessageForState(MasterViewHydratedCubitTypes state) {
+    switch (state) {
+      case MasterViewHydratedCubitTypes.loading:
+        return resources.loading;
+      case MasterViewHydratedCubitTypes.webview:
+        return resources.webview;
+      case MasterViewHydratedCubitTypes.error:
+        return resources.error;
+      case MasterViewHydratedCubitTypes.maintenance:
+        return resources.maintenance;
+      case MasterViewHydratedCubitTypes.empty:
+        return resources.empty;
+      case MasterViewHydratedCubitTypes.unauthorized:
+        return resources.unauthorized;
+      case MasterViewHydratedCubitTypes.timeout:
+        return resources.timeout;
+      default:
+        return '';
+    }
+  }
+
+  SnackBar _createSnackBar(MasterViewHydratedCubitTypes viewType) {
+    final message = _getSnackbarMessage(viewType);
+    return SnackBar(
+      behavior: SnackBarBehavior.floating,
+      content: Text(message),
+      duration: const Duration(days: 1),
+      action: SnackBarAction(
+        label: resources.undo,
+        onPressed: () {
+          debugPrint('Snackbar Undo pressed');
+          snackBarFunction();
+        },
+      ),
+    );
+  }
+
+  void _showSnackBar(BuildContext context, SnackBar snackBar) {
+    try {
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    } catch (e, s) {
+      debugPrint('Error showing snackbar: $e');
+      debugPrintStack(stackTrace: s);
+    }
+  }
+
+  static void defaultSnackBarFunction() {
+    debugPrint('Default Snackbar function called');
+  }
+
+  void navigateTo(BuildContext context, String path) {
+    GoRouter.of(context).go(path);
+  }
+
+  void navigateToWithArguments(
+      BuildContext context, String path, Map<String, dynamic> arguments) {
+    GoRouter.of(context).go(path, extra: arguments);
+  }
+}
+
+
