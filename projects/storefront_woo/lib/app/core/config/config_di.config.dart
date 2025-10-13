39fd7c25e5dcca10b02b3d0cb2ab1a4b574379dc
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2025-10-13 08:03:08 +0300

    feat: Update dependency injection configuration to include HomeViewModel and ProductDetailViewModel

--- projects/storefront_woo/lib/app/core/config/config_di.config.dart
+++ projects/storefront_woo/lib/app/core/config/config_di.config.dart
@@ -6,13 +6,15 @@
 // **************************************************************************
 
 // ignore_for_file: type=lint
-// coverage:ignore-file
+// coverage:ignore_file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
-import 'package:storefront_woo/app/views/product_catalog/models/product_catalog_view_model.dart'
+import 'package:storefront_woo/app/views/view_home/models/home_view_model.dart'
     as _i210;
+import 'package:storefront_woo/app/views/view_product_detail/models/product_detail_view_model.dart'
+    as _i571;
 
 extension GetItInjectableX on _i174.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -21,9 +23,10 @@ extension GetItInjectableX on _i174.GetIt {
     _i526.EnvironmentFilter? environmentFilter,
   }) {
     final gh = _i526.GetItHelper(this, environment, environmentFilter);
-    gh.factory<_i210.ProductCatalogViewModel>(
-      () => _i210.ProductCatalogViewModel(),
+    gh.factory<_i571.ProductDetailViewModel>(
+      () => _i571.ProductDetailViewModel(),
     );
+    gh.factory<_i210.HomeViewModel>(() => _i210.HomeViewModel());
     return this;
   }
 }
