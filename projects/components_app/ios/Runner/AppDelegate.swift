6b6481283fbc8cdfb8c7c083400dfc701c2e2df7
Berkay Vuranok <vuranokb@gmail.com>
2025-10-14 18:06:42 +0300

    feat: add: configure Google Maps API key integration in Android and iOS (Manifest, build.gradle, AppDelegate)

--- projects/components_app/ios/Runner/AppDelegate.swift
+++ projects/components_app/ios/Runner/AppDelegate.swift
@@ -1,12 +1,16 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Read the API key from the environment
+    let apiKey = ProcessInfo.processInfo.environment["GOOGLE_MAPS_API_KEY"]
+    GMSServices.provideAPIKey(apiKey ?? "YOUR_API_KEY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
